9903d28ea1b3618bdc51675bd60bf1af8fb70c45
Isnard Silva <isnardsilva@icloud.com>
2020-06-15 02:06:53 -0300

    Create base project

---
+++ CountdownApp/CountdownApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  CountdownApp WatchKit Extension
+//
+//  Created by Isnard Silva on 15/06/20.
+//  Copyright © 2020 Isnard Silva. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
